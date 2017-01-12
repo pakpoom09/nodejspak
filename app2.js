f2175d62e859960b8b1dd17c7911c04d5f810174
pakpoom <paku28049@gmail.com>
2017-01-12 11:54:41 +0700

    first commit

--- app2.js
+++ app2.js
@@ -1,36 +1,13 @@
-/*eslint-env node*/
-
-//------------------------------------------------------------------------------
-// node.js starter application for Bluemix
-//------------------------------------------------------------------------------
-
-// This application uses express as its web server
-// for more info, see: http://expressjs.com
-var express = require('express');
 var request = require("request");
 var app = require('express')();
 var bodyParser = require('body-parser');
-// cfenv provides access to your Cloud Foundry environment
-// for more info, see: https://www.npmjs.com/package/cfenv
-var cfenv = require('cfenv');
 
-// create a new express server
-var app = express();
-
-// serve the files out of ./public as our main files
-app.use(express.static(__dirname + '/public'));
+var port = process.env.PORT || 7777;
+// parse application/json
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
 }));
-// get the app environment from Cloud Foundry
-var appEnv = cfenv.getAppEnv();
-
-// start server on the specified port and binding host
-app.listen(appEnv.port, '0.0.0.0', function() {
-  // print a message when the server starts listening
-  console.log("server starting on " + appEnv.url);
-});
 
 app.get('/', function (req, res) {
     res.send('<h1>Hello Node.js</h1>');
@@ -58,4 +35,19 @@ app.post('/newuser', function (req, res) {
      res.send(x);
     });
 
+});
+/*
+async.map(entries, function(entry, cb) {
+  http.get(options, function(res) {
+    // call cb here, first argument is the error or null, second one is the result
+  })
+}, function(err, res) {
+  // this gets called when all requests are complete
+  // res is an array with the results
+}
+*/
+
+
+app.listen(port, function() {
+    console.log('Starting node.js on port ' + port);
 });
